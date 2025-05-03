0de2cb6335545c540cf8d091f79275f42e1f5216
Dayana Tey <dayanatey083@gmail.com>
2025-05-04 01:32:28 +0200

    Add demo page to view real-time configs (view+template)

--- config_app/views.py
+++ config_app/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.conf import settings
+from constance import config
+
+def home(request):
+    """
+    Demo page to view current constance configs.
+    """
+
+    config_keys = settings.CONSTANCE_CONFIG.keys()
+
+    # dict {key: current value}
+    configs = {}
+    for key in config_keys:
+        try:
+            configs[key] = getattr(config, key)
+        except Exception as e:
+            print(f"Could not get config for key '{key}'. Error: {e}")
+            configs[key] = None
+
+    context = {
+        'site_name': config.SITE_NAME,
+        'theme_color': config.THEME_COLOR,
+        'welcome_message': config.WELCOME_MESSAGE,
+        'maintenance_mode': config.MAINTENANCE_MODE,
+        'items_per_page': config.ITEMS_PER_PAGE,
+        'show_logs': config.SHOW_LOGS,
+        'configs': configs,
+    }
+
+    return render(request, 'config_app/home.html', context)
