eb10427adcd5121b19fcd04e4ab9beb513d529cb
Dayana Tey <dayanatey083@gmail.com>
2025-05-11 12:04:16 +0200

    Add init file for celery workers

---
+++ config_app/celery_init.py
@@ -0,0 +1,27 @@
+"""
+Import this when using Celery, to Celery config file (celery.py).
+"""
+
+import os
+import logging
+from celery.signals import worker_process_init
+
+logger = logging.getLogger(__name__)
+_worker_initialized_pids = {}
+
+@worker_process_init.connect(weak=False)
+def init_worker_process(sender=None, **kwargs):
+    """
+    Initialize celery worker with real-time config subscriber
+    """
+    pid: int = os.getpid()
+    if pid in _worker_initialized_pids:
+        logger.info(f"CELERY_WORKER_INIT PID {pid}: Already initialized, skipping.")
+        return
+    
+    from . import realtime_config
+    realtime_config.load_defaults()
+    realtime_config.start_subscriber_thread()
+    
+    _worker_initialized_pids[pid] = True
+    logger.info(f"Celery worker {pid} initialized with realtime_config")
